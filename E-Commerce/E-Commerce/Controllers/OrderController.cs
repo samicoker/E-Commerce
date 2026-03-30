e1eea212bf2a4863b5be301cdb1352deca41b231
samicoker <71135297+samicoker@users.noreply.github.com>
2026-03-30 03:35:55 +0300

    Order List Page Added in Admin Panel

---
+++ E-Commerce/E-Commerce/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using E_Commerce.Entity;
+using E_Commerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Commerce.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        DataContext db = new DataContext();
+        public ActionResult Index()
+        {
+            var orders = db.Orders.Select(x => new AdminOrder
+            {
+                Id = x.Id,
+                OrderNumber = x.OrderNumber,
+                OrderDate = x.OrderDate,
+                OrderState = x.OrderState,
+                Total = x.Total,
+                Count = x.OrderDetails.Count,
+            }).OrderByDescending(x=>x.OrderDate).ToList();
+
+            return View(orders);
+        }
+    }
+}

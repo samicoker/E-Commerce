a422b1764edf7feb423197e56cfc2df9dc68d67e
samicoker <71135297+samicoker@users.noreply.github.com>
2026-04-02 15:43:06 +0300

    Order statistics have been added to the admin panel

---
+++ E-Commerce/E-Commerce/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using E_Commerce.Entity;
+using E_Commerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Commerce.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        DataContext db = new DataContext();
+        public ActionResult Index()
+        {
+            StateModel model = new StateModel();
+
+            model.BekleyenSiparisCount=db.Orders.Where(x=>x.OrderState == OrderState.Bekleniyor).ToList().Count();
+            model.TamamlananSiparisCount=db.Orders.Where(x=>x.OrderState == OrderState.Tamamlandı).ToList().Count();
+            model.PaketlenenSiparisCount=db.Orders.Where(x=>x.OrderState == OrderState.Paketlendi).ToList().Count();
+            model.KargolananSiparisCount=db.Orders.Where(x=>x.OrderState == OrderState.Kargolandı).ToList().Count();
+
+            model.ProductCount = db.Products.Count();
+            model.OrderCount = db.Orders.Count();
+
+            return View(model);
+        }
+    }
+}
---
+++ E-Commerce/E-Commerce/Models/StateModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace E_Commerce.Models
+{
+    public class StateModel
+    {
+        public int ProductCount { get; set; }
+        public int OrderCount { get; set; }
+        public int BekleyenSiparisCount { get; set; }
+        public int TamamlananSiparisCount { get; set; }
+        public int PaketlenenSiparisCount { get; set; }
+        public int KargolananSiparisCount { get; set; }
+    }
+}
